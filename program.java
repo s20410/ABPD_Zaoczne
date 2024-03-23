0f79aedfb7e0395b4620f2eb60240a0c5fb6695a
s20410 <s20410@pjwstk.edu.pl>
2024-03-23 10:35:12 +0100

    Added program.java

--- program.java
+++ program.java
@@ -1,6 +1,5 @@
-public class Program{
-    public static void main(String[] args){
-        System.out.println("Czesc")
-        System.out.println("Narazie")
+public class program{
+    public static void main(String[] args) {
+        system.out.println("Hey");
     }
-}
+ }
